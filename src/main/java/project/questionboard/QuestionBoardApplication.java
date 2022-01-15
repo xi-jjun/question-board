dae0f6c0bdddad9bfe127e9ae1b1e9a4d7dffe1b
xi_jjun <rlawowns97@naver.com>
2022-01-15 17:43:31 +0900

    git init

---
+++ src/main/java/project/questionboard/QuestionBoardApplication.java
@@ -0,0 +1,13 @@
+package project.questionboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/questionboard/QuestionBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package project.questionboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
