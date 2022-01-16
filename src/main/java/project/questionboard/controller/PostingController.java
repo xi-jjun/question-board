46eac06be67b424781c70acee23f626aaeb3717c
xi_jjun <rlawowns97@naver.com>
2022-01-16 20:22:51 +0900

    refact: version1.1 controller

--- src/main/java/project/questionboard/controller/PostingController.java
+++ src/main/java/project/questionboard/controller/PostingController.java
@@ -1,21 +1,17 @@
 package project.questionboard.controller;
 
+import java.util.List;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import project.questionboard.domain.Posting;
-import project.questionboard.repository.PostingRepoMemoryImp;
 import project.questionboard.repository.PostingRepository;
-
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
 
 @Slf4j
 @Controller
@@ -34,19 +30,18 @@ public class PostingController {
 							  @RequestParam("code") String code,
 							  @RequestParam("writer") String writer,
 							  @RequestParam("deletePassword") String deletePassword) {
+		log.info("create posting");
+
 		Posting posting = new Posting(title, content, code, writer, deletePassword);
 		postingRepository.save(posting);
-		log.info("content = {}", content);
-		log.info("code = {}", code);
-		log.info("info = {}", postingRepository.findOne(posting.getId()));
 		return "redirect:/postings";
 	}
 
 	@GetMapping("/postings")
 	public String getPostings(Model model) {
-		log.info("postings info");
-		List<Posting> postings = postingRepository.findAll();
+		log.info("posting list");
 
+		List<Posting> postings = postingRepository.findAll();
 		model.addAttribute("postings", postings);
 		return "index";
 	}
@@ -54,13 +49,9 @@ public class PostingController {
 	@GetMapping("/postings/{postingId}")
 	public String getPosting(@PathVariable("postingId") String postingId, Model model) {
 		log.info("find Posting");
-		Posting posting = postingRepository.findOne(Integer.parseInt(postingId));
-		String content = posting.getContent();
-		String code = posting.getCode();
 
+		Posting posting = postingRepository.findOne(Integer.parseInt(postingId));
 		model.addAttribute("posting", posting);
-		model.addAttribute("contents", Arrays.asList(posting.getContent().split("\n")));
-		model.addAttribute("codes", Arrays.asList(posting.getCode().split("\n")));
 		return "posting";
 	}
 }
