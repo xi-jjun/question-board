8249dd942b7d5e09bff71745c078094db80660b6
xi_jjun <rlawowns97@naver.com>
2022-01-18 23:51:18 +0900

    refactor(controller/PostingController.java): add session check and change DI from repo to service

--- src/main/java/project/questionboard/controller/PostingController.java
+++ src/main/java/project/questionboard/controller/PostingController.java
@@ -10,17 +10,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import project.questionboard.domain.Posting;
-import project.questionboard.repository.PostingRepository;
+import project.questionboard.domain.User;
+import project.questionboard.service.PostingService;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class PostingController {
-	private final PostingRepository postingRepository;
+	private final PostingService postingService;
 
 	@GetMapping("/postings/new")
-	public String posting() {
+	public String posting(@SessionAttribute(name = SessionConstants.LOGIN_USER, required = false) User loginUser) {
+		if (loginUser == null) {
+			return "redirect:/";
+		}
 		return "addPostingForm";
 	}
 
@@ -28,29 +33,36 @@ public class PostingController {
 	public String postingForm(@RequestParam("title") String title,
 							  @RequestParam("content") String content,
 							  @RequestParam("code") String code,
-							  @RequestParam("writer") String writer,
-							  @RequestParam("deletePassword") String deletePassword) {
-		log.info("create posting");
+							  @SessionAttribute(name = SessionConstants.LOGIN_USER, required = false) User loginUser) {
+		if (loginUser == null) {
+			return "redirect:/";
+		}
 
-		Posting posting = new Posting(title, content, code, writer, deletePassword);
-		postingRepository.save(posting);
+		Posting newPosting = new Posting(title, content, code, loginUser);
+		postingService.posting(newPosting);
 		return "redirect:/postings";
 	}
 
 	@GetMapping("/postings")
-	public String getPostings(Model model) {
-		log.info("posting list");
+	public String getPostings(@SessionAttribute(name = SessionConstants.LOGIN_USER, required = false) User loginUser,
+							  Model model) {
+		if (loginUser == null) {
+			return "redirect:/";
+		}
 
-		List<Posting> postings = postingRepository.findAll();
+		List<Posting> postings = postingService.findAll();
 		model.addAttribute("postings", postings);
 		return "index";
 	}
 
 	@GetMapping("/postings/{postingId}")
-	public String getPosting(@PathVariable("postingId") String postingId, Model model) {
-		log.info("find Posting");
-
-		Posting posting = postingRepository.findOne(Integer.parseInt(postingId));
+	public String getPosting(@SessionAttribute(name = SessionConstants.LOGIN_USER, required = false) User loginUser,
+							 @PathVariable("postingId") String id, Model model) {
+		if (loginUser == null) {
+			return "redirect:/";
+		}
+		int postingId = Integer.parseInt(id);
+		Posting posting = postingService.findById(postingId);
 		model.addAttribute("posting", posting);
 		return "posting";
 	}
