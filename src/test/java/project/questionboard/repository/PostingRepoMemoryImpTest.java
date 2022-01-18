c2a63256e7b03d2dfe057e4fabd58b8bd509edee
xi_jjun <rlawowns97@naver.com>
2022-01-19 01:10:40 +0900

    test: 안쓰는 test 코드 에러나서 변경

--- src/test/java/project/questionboard/repository/PostingRepoMemoryImpTest.java
+++ src/test/java/project/questionboard/repository/PostingRepoMemoryImpTest.java
@@ -13,14 +13,14 @@ class PostingRepoMemoryImpTest {
 
 	@BeforeEach
 	void makeDummy() {
-		Posting posting1 = new Posting("제목1", "본문1", "코드1", "작성자1", "삭제비번1");
-		Posting posting2 = new Posting("제목2", "본문2", "코드2", "작성자2", "삭제비번2");
-		Posting posting3 = new Posting("제목3", "본문3", "코드3", "작성자3", "삭제비번3");
-		Posting posting4 = new Posting("제목4", "본문4", "코드4", "작성자4", "삭제비번4");
-		postingRepository.save(posting1);
-		postingRepository.save(posting2);
-		postingRepository.save(posting3);
-		postingRepository.save(posting4);
+//		Posting posting1 = new Posting("제목1", "본문1", "코드1", new );
+//		Posting posting2 = new Posting("제목2", "본문2", "코드2", "작성자2", "삭제비번2");
+//		Posting posting3 = new Posting("제목3", "본문3", "코드3", "작성자3", "삭제비번3");
+//		Posting posting4 = new Posting("제목4", "본문4", "코드4", "작성자4", "삭제비번4");
+//		postingRepository.save(posting1);
+//		postingRepository.save(posting2);
+//		postingRepository.save(posting3);
+//		postingRepository.save(posting4);
 	}
 
 	@Test
