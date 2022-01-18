9f2fb1f4315f113f43c241f76967888ff578250a
xi_jjun <rlawowns97@naver.com>
2022-01-18 22:58:40 +0900

    feat(MainController.java): add user session status

--- src/main/java/project/questionboard/controller/MainController.java
+++ src/main/java/project/questionboard/controller/MainController.java
@@ -1,12 +1,21 @@
 package project.questionboard.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import project.questionboard.domain.User;
 
 @Controller
 public class MainController {
 	@GetMapping("/")
-	public String home() {
-		return "index";
+	public String home(
+			@SessionAttribute(name = SessionConstants.LOGIN_USER, required = false) User loginUser,
+			Model model) {
+		if (loginUser == null) {
+			return "redirect:/login";
+		}
+
+		return "redirect:/postings";
 	}
 }
