410ea2429d0f5d8ec358c68c88f5c8b13ade7ce5
xi_jjun <rlawowns97@naver.com>
2022-01-18 23:30:42 +0900

    refactor(domain/Posting.java): add FK user_id and modify date by KOR
    User Entity를 추가함에 따라 Posting : User = 다 : 1 관계를 가진다.
    따라서 FK를 Posting Entity에 추가.
    -> 원래 있던 private String writer 라는 member variable 제거.
    -> 원래 있던 deletePassword 도 삭제.
    
    AWS 시간이 그리니치 천문대 기준인것을 감안하여 한국시로 +9H 를 해줬다.

--- src/main/java/project/questionboard/domain/Posting.java
+++ src/main/java/project/questionboard/domain/Posting.java
@@ -1,20 +1,23 @@
 package project.questionboard.domain;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import java.time.LocalDateTime;
 
 @Entity
-@Getter
+@Getter @RequiredArgsConstructor
 public class Posting {
-	@Id
+	@Id @Column(name = "posting_id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column
 	private int id;
 
 	@Column
@@ -26,25 +29,18 @@ public class Posting {
 	@Column
 	private String code;
 
-	@Column
-	private String writer;
-
-	@Column(name = "delete_pw")
-	private String deletePassword;
-
-	@Column(name = "create_dt")
+	@Column(name = "created_dt")
 	private LocalDateTime createDate;
 
-	public Posting(String title, String content, String code, String writer, String deletePassword) {
+	@ManyToOne(targetEntity = User.class, fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	public Posting(String title, String content, String code, User writer) {
 		this.title = title;
 		this.content = content;
 		this.code = code;
-		this.writer = writer;
-		this.deletePassword = deletePassword;
-		this.createDate = LocalDateTime.now();
-	}
-
-	public Posting() {
-
+		this.user = writer;
+		this.createDate = LocalDateTime.now().plusHours(9L);
 	}
 }
